9eb5959b7ffc2202b6a579b17c36a57f66ddf0b7
JAGDISHTOMCRUISE <jagdishparihar056@gmail.com>
2022-08-17 09:39:34 +0530

    ok
    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+/**typinf animation */
+var typed=new Typed (".typing",{
+
+    string:["web Designer","web Developer","graphic Designer","you Tuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+} )
